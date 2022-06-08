dd0c5ce9753b2ad05ed925eb52987964f0d9d881
Affan <57275933+afruse@users.noreply.github.com>
2022-06-08 13:29:10 -0400

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -16,7 +16,6 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(144, 256, 1); 
-        setBackground("background.png");
+        super(600, 400, 1); 
     }
 }
