c9ec26c707325771543f52f1872904f657a96a77
Affan <349980748@gapps.yrdsb.ca>
2022-06-15 20:48:49 -0400

    added Start class

---
+++ Start.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Start here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Start extends World
+{
+
+    
+    public Start()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(144, 256, 1, false);
+        setBackground("background.png");
+        
+    }
+}
